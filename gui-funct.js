1e4ef9873db97c8651570130e701063b240096d4
Anway <31155434+anwaysomani@users.noreply.github.com>
2021-03-07 21:49:10 +0530

    Stabilized code for desktop app

--- gui-funct.js
+++ gui-funct.js
@@ -1,53 +1,84 @@
-
 var is = require("electron-is");
-
-// Mac and Linux have Bash shell scripts (so the following would work)
-//        var child = process.spawn('child', ['-l']);
-//        var child = process.spawn('./test.sh');       
-// Win10 with WSL (Windows Subsystem for Linux)  https://docs.microsoft.com/en-us/windows/wsl/install-win10
-//   
-// Win10 with Git-Bash (windows Subsystem for Linux) https://git-scm.com/   https://git-for-windows.github.io/
-//
-
-function appendOutput(msg) { getCommandOutput().value += (msg+'\n'); };
-function setStatus(msg)    { getStatus().innerHTML = msg; };
+const dialog = require('electron');
 
 function showOS() {
-    if (is.windows())
-      appendOutput("Windows Detected.")
-    if (is.macOS())
-      appendOutput("Apple OS Detected.")
-    if (is.linux())
-      appendOutput("Linux Detected.")
+  if (is.windows()) console.log("Windows Detected")
+  if (is.macOS()) console.log("Apple OS Detected")
+  if (is.linux()) console.log("Linux Detected")
+}
+
+function startProcess(context) {
+  // 1-fe, 2-be, 3-auth, 4-reports, 5-notifs
+  switch (context) {
+    case 1:
+      console.log('Starting frontend war build')
+      break;
+
+    case 2:
+      console.log('Starting backend war build')
+      break;
+
+    case 3:
+      console.log('Starting auth war build')
+      break;
+
+    case 4:
+      console.log('Starting reports war build')
+      break;
+
+    case 5:
+      console.log('Starting notifications war build')
+      break;
+
+    default:
+      console.log('Error in building war')
+      break;
+
+  }
+}
+
+function fetchBranches(context) {
+  if (localStorage[context + '-file'] !== undefined)
+    showBranches(localStorage[context + '-file'], document.getElementById(context + '-dropdown'))
+  else
+    document.getElementById(context + '-dropdown').disabled = true;
+}
+
+function showBranches(dir, element) {
+  var process = require('child_process');
+  var cmd = ((is.windows()) ? 'test.bat' : './branch.sh');
+  var child = process.spawn(cmd, [dir]);
+
+  child.on('error', function (err) {
+    console.log(err.stack);
+  });
+
+  child.stdout.on('data', function (data) {
+    var set = data.toString().split(" ")
+    element.disabled = set.length <= 0;
+    element.length = 0;
+
+    for (var i = 0; i < set.length; i++) {
+      var opt = document.createElement("option")
+      opt.text = set[i]
+      element.add(opt)
+    }
+  });
+}
+
+function updatePath(context) {
+  localStorage[context + '-file'] = document.getElementById(context + '-file').files[0].path;
+  fetchBranches('frontend');
+}
+
+function init() {
+  fetchBranches('frontend')
+  fetchBranches('backend')
+  fetchBranches('auth')
+  fetchBranches('report')
+  fetchBranches('notifications')
 }
 
 function backgroundProcess() {
-    const process = require('child_process');   // The power of Node.JS
-
-    showOS();
-    var cmd = (is.windows()) ? 'test.bat' : './test.sh';      
-    console.log('cmd:', cmd);
-        
-    var child = process.spawn(cmd); 
-
-    child.on('error', function(err) {
-      appendOutput('stderr: <'+err+'>' );
-    });
-
-    child.stdout.on('data', function (data) {
-      appendOutput(data);
-    });
-
-    child.stderr.on('data', function (data) {
-      appendOutput('stderr: <'+data+'>' );
-    });
-
-    child.on('close', function (code) {
-        if (code == 0)
-          setStatus('child process complete.');
-        else
-          setStatus('child process exited with code ' + code);
-
-        getCommandOutput().style.background = "DarkGray";
-    });
-};
+  showOS();
+}
